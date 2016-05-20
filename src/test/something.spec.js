10a6d45f429f01c31e8966bfa858b50a130fec84
kconners <kconners@brierley.com>
2016-05-19 19:54:16 -0500

    Adding some tests and stuff
    DoneSkee

--- src/test/something.spec.js
+++ src/test/something.spec.js
@@ -5,3 +5,26 @@ var assert = require('chai').assert;
 
 
 var request = require('supertest');
+
+
+describe('This is something fun',
+function(){
+
+it("should verify that true is true",
+function(done){
+
+expect(true).to.be.true;
+done();
+
+});
+
+it("should verify that true is true",
+function(done){
+expect(false).to.be.true;
+done();
+
+
+
+});
+
+});
